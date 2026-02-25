ea20a2ad002d2d30a6085825cdfb42cba2f04ac6
Kristina Karantsevich <k.karantsevich@gmail.com>
2026-02-25 07:56:21 +0100

    feat: delete home page and add redirect to wheel page

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,8 +1,8 @@
-import { route, index } from '@react-router/dev/routes';
+import { route } from '@react-router/dev/routes';
 
 export default [
   route('/', './routes/layout.tsx', [
-    index('./routes/home.tsx'),
+    route('', './routes/RedirectToWheel.tsx'),
     route('wheel', './routes/WheelRoute.tsx'),
     route('coin', './routes/CoinRoute.tsx'),
     route('dice', './routes/DiceRoute.tsx'),
